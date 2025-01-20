c277c3d33cfb684d61fbcb457ce3f8508ed95a7d
2arnav4 <singlaarnav2405@gmail.com>
2025-01-20 18:30:03 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"], // Replaces `purge`
+  darkMode: false, // Options: 'media' or 'class'
+  theme: {
+    extend: {}, // Add custom styles here
+  },
+  variants: {
+    extend: {}, // Add custom variant styles here
+  },
+  plugins: [], // Add plugins if needed
+};
